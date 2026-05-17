04f32c318a1120f4c6fcd201fc51ceb9978cc856
akashyadavam <akashyadavam585ak@gmail.com>
2026-05-18 00:47:27 +0530

    closer added

--- MyNotes/script.js
+++ MyNotes/script.js
@@ -31,27 +31,137 @@
 
 // closure;
 
-function x(){
-    var a=7;
-    function y(){
-        console.log(a);
+// function x(){
+//     var a=7;
+//     function y(){
+//         console.log(a);
 
-    }
-    a=100;
-    return y;
-}
-var z=x();
-console.log(z);
-z();//100
-
-
+//     }
+//     a=100;
+//     return y;
+// }
+// var z=x();
+// console.log(z);
+// z();//100
 
 
 
+// function x() {
+
+//     for (var i = 1; i <= 5; i++) {
+
+//         function close(x) {
+
+//             setTimeout(function () {
+//                 console.log(x);
+//             }, x * 1000);
+//         }
+
+//         close(i);
+//     }
+
+//     console.log("Namaste JavaScript");
+// }
+
+// x();//output 1 2 3 4 5 6
 
 
 
 
+// function x() {
+
+//     
+// for(var i=1;i<=5;i++){
+
+//    setTimeout(function(){
+//       console.log(i);
+//    },1000);
+// }
+//     }
+// }
+    
+// x();// 6 6 6 6 6 6
+
+
+// function x() {
+
+// for(let i=1;i<=5;i++){
+
+//    setTimeout(function(){
+//       console.log(i);
+//    },1000);
+// }
+// }
+// x();//1 2 3 4 5 
+
+// function outer(){
+
+//     function inner(){
+//         console.log(a);
+//     }
+
+//     var a = 10;
+
+//     return inner;
+// }
+
+// var close = outer();
+
+// close();
+
+
+// function outest(){
+
+//     var c = 20;
+
+//     function outer(b){
+
+//         function inner(){
+//             console.log(a, b, c);
+//         }
+
+//         let a = 10;
+
+//         return inner;
+//     }
+
+//     return outer;
+// }
+
+// let a = 100;
+
+// var close = outest()("helloworld");
+
+// close();//current scope
+// ↓
+// outer scope
+// ↓
+// outer's outer scope
+// ↓
+// global
+
+
+
+
+//     function outer(b){
+
+//         function inner(){
+//             console.log(a, b, c);
+//         }
+
+//         // let a = 10;
+
+//         return inner;
+//     }
+
+//     return outer;
+// }
+
+// let a = 100;
+
+// var close = outest()("helloworld");
+
+// close();
 
 
 
